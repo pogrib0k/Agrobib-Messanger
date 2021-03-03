ce0e8d6738fd13456370e840c23d4b871f27d7db
Olexandr <nadtochiy1915@gmail.com>
2021-03-03 22:13:29 +0200

    [iss#6] Added email/password authentication

--- app/src/main/java/com/nubip/agrobib_messenger/ChatActivity.kt
+++ app/src/main/java/com/nubip/agrobib_messenger/ChatActivity.kt
@@ -1,11 +1,38 @@
 package com.nubip.agrobib_messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_chat.*
+// TODO: remake all
+class ChatActivity : AppCompatActivity(), View.OnClickListener {
 
-class ChatActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        auth = Firebase.auth
+
+        logout_button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.logout_button -> signOut()
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+
+        val chatActivity = Intent(this, LoginActivity::class.java)
+        startActivity(chatActivity)
+        finish()
     }
 }
--- app/src/main/java/com/nubip/agrobib_messenger/LoginActivity.kt
+++ app/src/main/java/com/nubip/agrobib_messenger/LoginActivity.kt
@@ -1,11 +1,93 @@
 package com.nubip.agrobib_messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        login_button.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.login_button -> {
+                val email = editTextEmail.text.toString()
+                val password = editTextPassword.text.toString()
+                signIn(email, password)
+            }
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        startMessenger()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = editTextEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.error = "Required."
+            valid = false
+        } else {
+            editTextEmail.error = null
+        }
+
+        val password = editTextPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.error = "Required."
+            valid = false
+        } else {
+            editTextPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun startMessenger() {
+        val intent = Intent(this, ChatActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        private val TAG = LoginActivity::class.java.simpleName
+    }
+
 }
